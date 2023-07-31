437b293a54a892e7ab2e7e55197c9805da2e815b
unknown <dinabeysinghe@gmail.com>
2023-07-31 13:02:09 +0530

    create class base componets in react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div>
+      Hi ! class base componets working
+    </div>;
+  }
+}
+export default App;
